80b2219c00525348adaf0e0964caf44e7680e74a
jeyakaran <mjeyakaran@gmail.com>
2013-05-24 11:01:19 +0800

    Preject Structure

---
+++ Test/src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jeyakaran
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
